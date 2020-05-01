b889cc4b03fc23b4f47f44cc9a16aafcb91567b6
HINA KHADIM <hinakhadim2002@gmail.com>
2020-05-01 21:39:36 +0500

    set coverphoto and update

--- users/profile.php
+++ users/profile.php
@@ -3,7 +3,7 @@
     session_start();
     include('../includes/header.php');
 
-    if(!isset($_SESSION['user_email'])){
+    if(isset($_SESSION['user_email'])){
         header('location:index.php');
     }
 ?>
@@ -25,6 +25,52 @@
     <title><?php echo "$user_name"; ?></title>
 </head>
 <body>
-    
+    <div>
+        <div>
+
+        </div>
+        <div>
+            <?php
+                echo"
+                    <div>
+                        <div><img id='cover-img' src='cover/$user_cover' alt='cover></div>
+                        <form action='profile.php?u_id=$user_id' method='post' enctype='multipart/form-data' >
+                            <ul style='position : absolute; top:10px; left:40px;'>
+                                <li>
+                                    <button>
+                                    Change Cover
+                                    </button>
+                                    <div>
+                                        <center>
+                                            <p>Click <strong>Select Cover</strong> and then click the 
+                                            <br> <strong>Update Cover</strong></p>
+                                            <label>Select Cover Image
+                                            <input type='file' name='u_cover' size='60' />
+
+                                            </label><br><br>
+                                            <button name='submit'>Update Cover</button>
+                                        </center>
+                                    </div>
+                                </li>
+                            </ul>
+                        </form>
+                    </div>
+
+                    <div>
+                        <img src='users/$user_image' alt='Profile' width='180px' height='185px' >
+                        <form action='profile.php?u_id='$user_id' method='post' enctype='multipart/form-data' >
+
+                            <label>Select Cover
+                            <input type='file' name='u_cover' size='60 />
+                            </label><br><br>
+
+                            <button name='submit' >Update Cover</button>
+
+                        </form>
+                    </div>
+                ";
+            ?>
+        </div>
+    </div>
 </body>
 </html>
