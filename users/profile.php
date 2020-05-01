c60ca5cee9d157342a60bf06d6c126e3453b55c0
HINA KHADIM <hinakhadim2002@gmail.com>
2020-05-01 22:16:27 +0500

    set profile image and update

--- users/profile.php
+++ users/profile.php
@@ -60,17 +60,75 @@
                         <img src='users/$user_image' alt='Profile' width='180px' height='185px' >
                         <form action='profile.php?u_id='$user_id' method='post' enctype='multipart/form-data' >
 
-                            <label>Select Cover
-                            <input type='file' name='u_cover' size='60 />
+                            <label>Select Profile
+                            <input type='file' name='u_image' size='60 />
                             </label><br><br>
 
-                            <button name='submit' >Update Cover</button>
+                            <button name='update' >Update Profile</button>
 
                         </form>
-                    </div>
+                    </div><br>
+
                 ";
             ?>
+
+            <?php
+                if(isset($_POST['submit'])){
+                    $u_cover= $_FILES['u_cover']['name'];
+                    $image_tmp = $_FILES['u_cover']['tmp_name'];
+                    $random_number = rand(1,100);
+
+                    if($u_cover==''){
+                        echo "<script>alert('Please select Cover Image')</script>";
+                        echo "<script>window.open('profile.php?u_id=$user_id', '_self')</script>";
+
+                        exit();
+                    }
+                    else{
+                        move_uploaded_file($image_tmp, "cover/$u_cover.$random_number");
+                        $update = "update users set user_cover = '$u_cover.$random_number' where user_id='$user_id'";
+
+                        $run = mysqli_query($con, $update);
+
+                        if($run){
+                            echo "<script>alert('Your Cover Updated')</script>";
+                            echo "<script>window.open('profile.php?u_id=$user_id', '_self')</script>";
+                        }
+                    }
+                }
+            ?>
         </div>
+
+        <?php
+            if(isset($_POST['update'])){
+                $u_image= $_FILES['u_image']['name'];
+                $image_tmp = $_FILES['u_image']['tmp_name'];
+                $random_number = rand(1,100);
+
+                if($u_image==''){
+                    echo "<script>alert('Please select Profile Image on clicking on your Profile Image')</script>";
+                    echo "<script>window.open('profile.php?u_id=$user_id', '_self')</script>";
+
+                    exit();
+                }
+                else{
+                    move_uploaded_file($image_tmp, "users/$u_image.$random_number");
+                    $update = "update users set user_image = '$u_image.$random_number' where user_id='$user_id'";
+
+                    $run = mysqli_query($con, $update);
+
+                    if($run){
+                        echo "<script>alert('Your Profile Updated')</script>";
+                        echo "<script>window.open('profile.php?u_id=$user_id', '_self')</script>";
+                    }
+                }
+            }
+        ?>
+
+        <div>
+            
+        </div>
+
     </div>
 </body>
 </html>
