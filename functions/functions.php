919063978d77bd1449552ea390245db91ef74b00
Vazeema-Siddiqui <57322838+Vazeema-Siddiqui@users.noreply.github.com>
2020-05-06 07:27:11 +0500

    Update functions.php

--- functions/functions.php
+++ functions/functions.php
@@ -35,7 +35,7 @@ function insertPost(){
                 exit();
             }
             else{
-                if($upload_image= '' && $content == ''){
+                if($upload_image == '' && $content == ''){
                     echo "<script>alert('Error occured while uploading! ')</script>";
                     echo "<script>window.open('home.php', '_self')</script>";
                 }
@@ -56,7 +56,7 @@ function insertPost(){
                         exit();
                     }
                     else{
-                        $insert = "insert into posts (user_id, post_content, post_date) values('$user_id', 'No',NOW())";
+                        $insert = "insert into posts (user_id, post_content, post_date) values('$user_id', '$content', NOW())";
 
                         $run = mysqli_query($con, $insert);
 
@@ -72,5 +72,137 @@ function insertPost(){
         }
     }
 }
+// <Vazeema>
+function get_posts(){
+    global $con;
+    $per_page = 4;
+
+    if(isset(&_GET['page'])){
+        $page = $_GET['page'];
+    }
+    else{
+        $page = 1;
+    }
+
+    $start_from = ($page-1) * $per_page;
+    $get_posts = "select * from posts ORDER by 1 DESC LIMIT $start_from, $per_page";
+    $run_posts = mysqli_query($con, $get_posts);
+
+    while($row_posts = mysqli_fetch_array($run_posts)){
+        $post_id = $row_posts['post_id'];
+        $user_id = $row_posts['user_id'];
+        $content = substr($row_posts['post_content'], 0,40);
+        $upload_image = $row_posts['upload_image'];
+        $post_date = $row_posts['post_date'];
+        
+        $user = "select * from users where user_id='$user_id' AND posts='yes'";
+        $run_user = mysqli_query($con, $user);
+        $row_user = mysqli_fetch_array($run_user);
+
+        $user_name = $row_user['user_name'];
+        $user_image = $row_user['user_image'];
+
+        //displaying posts from database 
+
+        if($content == "No" && strlen($upload_image) >= 1){
+            echo"
+            <div class='row>
+                <div class='col-sm-3'>
+                </div>
+
+                <div id='posts' class='col-sm-6'>
+                    <div class='row'>
+                        <div class='col-sm-2'>
+                            <p><img src='users/$user_image' class='img-circle' width='100px' height='100px'></p>
+                        </div>
+                        <div class='col-sm-6'>
+                            <h3><a style='text-decoration:none; cursor:pointer; color:#3897f0;' href='user_profile.php?u_id=$user_id'>$user_name</h3>
+                            <h4><small style='color:black;'>Updated a post on <strong>$post_date</strong></small></h4>
+                        </div>
+                        <div class='col-sm-4>
+                        </div>
+                    </div>
+                    <div class='row'>
+                        <div class='col-sm-12'>
+                            <img id='posts-img' src='imagepost/$upload_image' style='height:350px;'>
+                        </div>
+                    </div><br>
+                    <a href='single.php?post_id=$post_id' style='float:right;'><button class='btn btn-info'>Comment</button></a><br>
+                </div>
+                
+                <div class='col-sm-3'>
+                </div>
+			</div><br><br>
+            ";
+        }
+
+        else if(strlen($content) >= 1 && strlen($upload_image) >= 1){
+            echo"
+            <div class='row>
+                <div class='col-sm-3'>
+                </div>
+
+                <div id='posts' class='col-sm-6'>
+                    <div class='row'>
+                        <div class='col-sm-2'>
+                            <p><img src='users/$user_image' class='img-circle' width='100px' height='100px'></p>
+                        </div>
+                        <div class='col-sm-6'>
+                            <h3><a style='text-decoration:none; cursor:pointer; color:#3897f0;' href='user_profile.php?u_id=$user_id'>$user_name</h3>
+                            <h4><small style='color:black;'>Updated a post on <strong>$post_date</strong></small></h4>
+                        </div>
+                        <div class='col-sm-4>
+                        </div>
+                    </div>
+                    <div class='row'>
+                        <div class='col-sm-12'>
+                            <p>$content</p>
+                            <img id='posts-img' src='imagepost/$upload_image' style='height:350px;'>
+                        </div>
+                    </div><br>
+                    <a href='single.php?post_id=$post_id' style='float:right;'><button class='btn btn-info'>Comment</button></a><br>
+                </div>
+                
+                <div class='col-sm-3'>
+                </div>
+			</div><br><br>
+            ";
+        }
+        else{
+            echo"
+            <div class='row>
+                <div class='col-sm-3'>
+                </div>
+
+                <div id='posts' class='col-sm-6'>
+                    <div class='row'>
+                        <div class='col-sm-2'>
+                            <p><img src='users/$user_image' class='img-circle' width='100px' height='100px'></p>
+                        </div>
+                        <div class='col-sm-6'>
+                            <h3><a style='text-decoration:none; cursor:pointer; color:#3897f0;' href='user_profile.php?u_id=$user_id'>$user_name</h3>
+                            <h4><small style='color:black;'>Updated a post on <strong>$post_date</strong></small></h4>
+                        </div>
+                        <div class='col-sm-4>
+                        </div>
+                    </div>
+                    <div class='row'>
+                        <div class='col-sm-12'>
+                            <h3><p>$content</p></h3>
+                        </div>
+                    </div><br>
+                    <a href='single.php?post_id=$post_id' style='float:right;'><button class='btn btn-info'>Comment</button></a><br>
+                </div>
+                
+                <div class='col-sm-3'>
+                </div>
+			</div><br><br>
+            "
+        }
+    }
+
+    include("pagination.php");
+}
+// </Vazeema>
 
 ?>
