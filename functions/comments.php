3337c37335eaf66ec78c344c45feac0c0ce3e124
Laviza Falak Naz <60230431+LavizaFalakNaz@users.noreply.github.com>
2020-08-22 16:41:03 +0500

    Update comments.php

---
+++ functions/comments.php
@@ -0,0 +1,33 @@
+<!-- Laviza falak naz-->
+
+<?php
+	
+	$get_id = $_GET['post_id'];
+	$get_com = "select * from comments where post_id = '$get_id' ORDER BY 1 DESC";
+
+	$ run_com = mysqli_query($con, $get_com);
+
+	while($row = mysqli_fetch_array($run_com))
+	{
+		$com = $row['comment'];
+		$com_name = $row['comment_authot'];
+		$date = $row['date'];
+
+		echo"
+
+			<div class='row'>
+				<div class='col-md-6 col md-offset-3'>
+                            <div class='panel panel-info'>
+                                <div class='panel-body'>
+                                    <div>
+                                    	<h4><strong>$com_name</strong><i>commented</i> on $date</h4>
+                                    	<p class='text-primary' style='margin-left:5px; font-size:20px;'>$com</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+			</div>
+		";
+	}
+
+?>
